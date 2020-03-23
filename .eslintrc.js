71183e05cf4b78bb58b3d802b88cb0c3d7a978c4
xxlabaza <xxlabaza@gmail.com>
2020-03-23 17:37:02 +0300

    fix eslint file's identation

--- .eslintrc.js
+++ .eslintrc.js
@@ -16,63 +16,63 @@
  */
 
 module.exports = {
-    "env": {
-      "commonjs": true,
-      "es6": true,
-      "node": true
-    },
-    "extends": "eslint:recommended",
-    "globals": {
-      "Atomics": "readonly",
-      "SharedArrayBuffer": "readonly"
-    },
-    "parserOptions": {
-      "ecmaVersion": 2018
-    },
-    "rules": {
-      "no-console": [
-        "error",
-        { "allow": ["warn", "error"] }
-      ],
-      "indent": [
-        "error",
-        2
-      ],
-      "linebreak-style": [
-        "error",
-        "unix"
-      ],
-      "quotes": [
-        "error",
-        "double"
-      ],
-      "semi": [
-        "error",
-        "always"
-      ],
-      "space-before-function-paren": [
-        "error",
-        { "anonymous": "always", "named": "always", "asyncArrow": "always" }
-      ],
-      "space-in-parens": [
-        "error",
-        "never"
-      ],
-      "space-before-blocks": [
-        "error",
-        "always"
-      ],
-      "arrow-spacing": [
-        "error",
-        { "before": true, "after": true }
-      ],
-      "space-infix-ops": [
-        "error",
-        { "int32Hint": false }
-      ],
-      "keyword-spacing": [
-        "error",
-        { "before": true, "after": true }
-      ]
-    }
-  };
+  "env": {
+    "commonjs": true,
+    "es6": true,
+    "node": true
+  },
+  "extends": "eslint:recommended",
+  "globals": {
+    "Atomics": "readonly",
+    "SharedArrayBuffer": "readonly"
+  },
+  "parserOptions": {
+    "ecmaVersion": 2018
+  },
+  "rules": {
+    "no-console": [
+      "error",
+      { "allow": ["warn", "error"] }
+    ],
+    "indent": [
+      "error",
+      2
+    ],
+    "linebreak-style": [
+      "error",
+      "unix"
+    ],
+    "quotes": [
+      "error",
+      "double"
+    ],
+    "semi": [
+      "error",
+      "always"
+    ],
+    "space-before-function-paren": [
+      "error",
+      { "anonymous": "always", "named": "always", "asyncArrow": "always" }
+    ],
+    "space-in-parens": [
+      "error",
+      "never"
+    ],
+    "space-before-blocks": [
+      "error",
+      "always"
+    ],
+    "arrow-spacing": [
+      "error",
+      { "before": true, "after": true }
+    ],
+    "space-infix-ops": [
+      "error",
+      { "int32Hint": false }
+    ],
+    "keyword-spacing": [
+      "error",
+      { "before": true, "after": true }
+    ]
+  }
+};
